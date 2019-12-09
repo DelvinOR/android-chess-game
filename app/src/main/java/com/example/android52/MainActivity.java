3149b28fa5ebadb8348cabaf44126df9632a479d
Shazim Chaudhary <35382434+ShazimC@users.noreply.github.com>
2019-12-09 06:50:33 -0500

    main activity

--- app/src/main/java/com/example/android52/MainActivity.java
+++ app/src/main/java/com/example/android52/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Controller.main(null);
+        //Controller.main(null);
         setContentView(R.layout.activity_main);
     }
 }
