1a2f41687542cabcbc45ea2cecaa67928d331829
Delvin Ortiz <delvin732@gmail.com>
2019-12-10 15:31:46 -0500

    Implemented game playback for user

--- app/src/main/java/com/example/android52/GameReplay.java
+++ app/src/main/java/com/example/android52/GameReplay.java
@@ -1,12 +1,28 @@
 package com.example.android52;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class GameReplay extends AppCompatActivity {
+
+    public VideoView videoView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_replay_layout);
+
+        videoView = (VideoView) findViewById(R.id.video_view);
+        videoView.setVideoURI(Uri.parse(RealMainActivity.mVideoURL));
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+
+        // We will need an anchor view for our media controller
+        mediaController.setAnchorView(videoView);
     }
 }
